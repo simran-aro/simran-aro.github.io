0e7f442f25eb762ca0b84b2f1e8e8c98eb184a2a
sim-py <98434506+sim-py@users.noreply.github.com>
2022-11-09 15:16:43 -0500

    Update

--- js/school-map.js
+++ js/school-map.js
@@ -4,13 +4,14 @@ import catchments from '../data/catchments.js';
 function initializeSchoolMap() {
     let schoolMap = L.map('school-map').setView([39.95248581284671, -75.16445594067633], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20,
     }).addTo(schoolMap);
 
     const phl = L.geoJSON(catchments, {
-        style: { fill: null, color: 'black', weight:1 },
+        style: { fill: "#127782", fillOpacity: 0.05, color: '#127782', weight:1.5 },
     }).addTo(schoolMap);
 
     schoolMap.fitBounds(phl.getBounds());
@@ -44,9 +45,9 @@ function showSchoolsOnMap(schoolsToShow, schoolMap) {
     schoolMap.schoolLayers = L.geoJSON(schoolFeatureCollection, {
         pointToLayer: (geoJsonPoint, latlng) => L.circleMarker(latlng),
         style: {
-            stroke: 0.25,
-            fillOpacity: 0.5,
-            radius: 2.5,
+            stroke: 0.1,
+            fillOpacity: 0.25,
+            radius: 2,
         },
     })
     .bindTooltip(layer => layer.feature.properties['name'])
